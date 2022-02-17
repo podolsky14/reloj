98dba07d39ce6b2a5c6bfa3cf32576a6492a8b00
podolsky14 <maxi_ldm@hotmail.com>
2022-02-17 15:42:44 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./index.css"
+
+
+
+import BuscadorApi from "./components/BuscadorApi";
+
+//import Ejemplo from "./components/Ejemplo";
+
+function App() {
+
+
+
+
+
+
+
+
+  return (
+    <>
+    <BuscadorApi/>
+
+  
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/BuscadorApi.js
@@ -0,0 +1,89 @@
+import { useEffect, useState } from "react";
+import "../index.css"
+
+
+export default function BuscadorApi() {
+const[usuario, setUsuario] = useState([])
+const[busqueda, setBusqueda] = useState([])
+const [tablaUsuarios, setTablaUsuarios]= useState([]);
+
+useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users").then(res=>{
+        if(res.ok)
+        return res.json()
+        else
+        throw (res)
+      })
+      .then(res=>{
+        setUsuario(res)
+        setTablaUsuarios(res);
+    
+      })
+      .catch(res=>{
+        setUsuario(null)
+      })
+
+  }, [])
+
+  const onChange = (e) => {
+    setBusqueda(e.target.value);
+    filtrar(e.target.value);
+  }
+
+  const filtrar=(terminoBusqueda)=>{
+    var resultadosBusqueda= tablaUsuarios.filter((elemento)=>{
+      if(elemento.name.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
+      || elemento.company.name.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
+      ){
+        return elemento;
+      }
+    });
+    setUsuario(resultadosBusqueda);
+  }
+
+  
+
+
+  return (
+    <>
+<form>
+<input value={busqueda} onChange={onChange}/>
+<button>BUSCAR</button>
+</form>
+
+
+       <table className="table border border-primary">
+             <thead>
+               <tr>
+                 <th >ID</th>
+                 <th >Nombre</th>
+                 <th >Teléfono</th>
+                 <th >Correo</th>
+               </tr>
+             </thead>
+    
+             <tbody>
+               {
+               usuario.map((el)=>(
+                 <tr key={el.id}>
+                   <td>{el.id}</td>
+                   <td>{el.name}</td>
+                   <td>{el.phone}</td>
+                   <td>{el.email}</td>
+                 </tr>
+               ))}
+             </tbody>
+    
+           </table>
+    
+     
+     
+     
+     
+     
+     </>
+
+
+
+  )
+}
