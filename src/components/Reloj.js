7dac0dd8a1f561e230b87e3b00ba626c51d9271c
Maximiliano Calero <93091450+podolsky14@users.noreply.github.com>
2022-03-02 09:32:03 -0300

    Update Reloj.js

--- src/components/Reloj.js
+++ src/components/Reloj.js
@@ -1,18 +1,18 @@
 import React, { useState } from 'react'
 import "../App.css"
-const data = ""
+
 export default function Reloj() {
-    const [state, setstate] = useState(data)
+    const [state, setstate] = useState(null)
  
 setInterval(() => {
     setstate({
-        data : new Date().toLocaleTimeString()
+        state : new Date().toLocaleTimeString()
     })
 }, 1000);
    
   return (
-    <div className='border border-primary'>
-<h1 className='reloj'>{state.data}</h1>
+ <div className='border border-primary'>
+<h1 className='reloj'>{state}</h1>
     </div>
   )
 }
